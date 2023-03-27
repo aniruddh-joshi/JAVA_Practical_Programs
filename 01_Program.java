c2fe4503f4d661409b8513759c73855fd5fccfd4
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-03-27 17:01:21 +0000

    Use of Command Line Argument

---
+++ 01_Program.java
@@ -0,0 +1,6 @@
+class demo{
+    public static void main(String ar[]){
+        System.out.println("The First Command Line Argument is "+ar[0]);
+        System.out.println("The Second Command Line Argument is "+ar[1]);
+    }
+}
